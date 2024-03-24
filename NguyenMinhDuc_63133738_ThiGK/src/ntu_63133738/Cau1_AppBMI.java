29eb3f91c08bce2c8effefc48521d60a161b454e
Nguyễn Minh Đức - 63133738 <157378417+NguyenMinhDuc63133738@users.noreply.github.com>
2024-03-24 10:18:18 +0700

    ThiGK

--- NguyenMinhDuc_63133738_ThiGK/src/ntu_63133738/Cau1_AppBMI.java
+++ NguyenMinhDuc_63133738_ThiGK/src/ntu_63133738/Cau1_AppBMI.java
@@ -32,11 +32,14 @@ public class Cau1_AppBMI extends JFrame {
 	 */
 	public Cau1_AppBMI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 600, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		
 	}
 
 }
