4280fb48e6c20b252470a79f20cd7dfc04c6f15f
Nguyễn Minh Đức - 63133738 <157378417+NguyenMinhDuc63133738@users.noreply.github.com>
2024-03-26 00:17:12 +0700

    Update MainActivity.java

--- ThiGK_63133738/Cau3_AppGioiThieu/app/src/main/java/ntu_63133738/cau3_appgioithieu/MainActivity.java
+++ ThiGK_63133738/Cau3_AppGioiThieu/app/src/main/java/ntu_63133738/cau3_appgioithieu/MainActivity.java
@@ -3,6 +3,7 @@ package ntu_63133738.cau3_appgioithieu;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,24 +12,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    EditText HovaTen, MSSV,Lop,Chuyennganh,Email;
+    TextView HovaTen, MSSV,Lop,Chuyennganh,Email;
     Button btnHovaTen, btnMSSV, btnLop, btnChuyennganh, btnEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        HovaTen = findViewById(R.id.et_Hovaten);
-        MSSV = findViewById(R.id.et_Hovaten);
-        Lop = findViewById(R.id.et_Hovaten);
-        Chuyennganh = findViewById(R.id.et_Hovaten);
-        Email = findViewById(R.id.et_Hovaten);
-        btnHovaTen = findViewById(R.id.et_Hovaten);
-        btnMSSV = findViewById(R.id.et_Hovaten);
-        btnLop = findViewById(R.id.et_Hovaten);
-
-
-
-
+        HovaTen = findViewById(R.id.txt_hovaten);
+        MSSV = findViewById(R.id.txt_mssv);
+        Lop = findViewById(R.id.txt_lop);
+        Chuyennganh = findViewById(R.id.txt_chuyennganh);
+        Email = findViewById(R.id.txt_email);
+        btnHovaTen = findViewById(R.id.btn_hovaten);
+        btnMSSV = findViewById(R.id.btn_mssv);
+        btnLop = findViewById(R.id.btn_lop);
+        btnChuyennganh = findViewById(R.id.btn_cn);
+        btnEmail = findViewById(R.id.btn_email);
     }
 }
