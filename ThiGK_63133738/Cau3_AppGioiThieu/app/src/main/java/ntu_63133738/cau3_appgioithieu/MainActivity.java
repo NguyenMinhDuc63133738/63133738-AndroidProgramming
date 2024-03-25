162c019a6293b242494b038a59a498a9f35f1cc4
Nguyễn Minh Đức - 63133738 <157378417+NguyenMinhDuc63133738@users.noreply.github.com>
2024-03-26 00:39:11 +0700

    update

--- ThiGK_63133738/Cau3_AppGioiThieu/app/src/main/java/ntu_63133738/cau3_appgioithieu/MainActivity.java
+++ ThiGK_63133738/Cau3_AppGioiThieu/app/src/main/java/ntu_63133738/cau3_appgioithieu/MainActivity.java
@@ -1,9 +1,12 @@
 package ntu_63133738.cau3_appgioithieu;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +17,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     TextView HovaTen, MSSV,Lop,Chuyennganh,Email;
     Button btnHovaTen, btnMSSV, btnLop, btnChuyennganh, btnEmail;
+    RadioButton rbXacnhan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +33,20 @@ public class MainActivity extends AppCompatActivity {
         btnLop = findViewById(R.id.btn_lop);
         btnChuyennganh = findViewById(R.id.btn_cn);
         btnEmail = findViewById(R.id.btn_email);
+        rbXacnhan = findViewById(R.id.rb_xacnhan);
+
+        btnHovaTen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (rbXacnhan.isChecked()) {
+                    HovaTen.setText("Nguyễn Minh Đức");
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Vui lòng chọn xác nhận!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        
+
     }
 }
